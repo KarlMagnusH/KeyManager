ee91852442507a6c760db4deb7f22e48dc2cdd53
Karl <Magnushovmand@gmail.com>
2025-11-12 09:20:15 +0100

    rettelser af interface til add_bk_for_table + ændring af test

--- keys/utility.py
+++ keys/utility.py
@@ -5,17 +5,17 @@ from .key_manager import (
     BK_SEP
 )
 
-def set_business_key(df_incoming: pd.DataFrame, *columns: str, table_name: Optional[str] = None, bk_name: Optional[str] = None):
-    if bk_name is None and table_name is None:
-        raise ValueError("Must provide either bk_name or table_name")
-    if bk_name is None:
-        bk_name = f"bk_{table_name}"
+def add_bk_for_table(for_table: str, df: pd.DataFrame, *columns: str, bk_prefix="bk") -> pd.Series:
+    """
+    Adds a buiness key related to a given table. The table can either be the table it self or a related table
+    """
 
+    bk_name = f"{bk_prefix}_{for_table}"
     if not columns:
         raise ValueError("Must provide at least one column for business key.")
-    columns_missing = [c for c in columns if c not in df_incoming.columns]
+    columns_missing = [c for c in columns if c not in df.columns]
     if columns_missing:
         raise ValueError(f"Business key source columns missing in incoming df: {columns_missing}")
-    df_bk_cols_as_string = df_incoming[list(columns)].astype(str).fillna("")
-    df_incoming[bk_name] = df_bk_cols_as_string.agg(BK_SEP.join, axis=1)
-    return df_incoming[bk_name]
+    df_bk_cols_as_string = df[list(columns)].astype(str).fillna("")
+    df[bk_name] = df_bk_cols_as_string.agg(BK_SEP.join, axis=1)
+    return df[bk_name]
--- test/test_utility.py
+++ test/test_utility.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 
-from keys.utility import set_business_key
+from keys.utility import add_bk_for_table
 from CaseGen import CaseGen, BUILTINS
 
 @pytest.fixture
@@ -10,33 +10,16 @@ def test_data():
     return CaseGen().add_dict(BUILTINS)
 
 class TestBuisnessKey:
-
-    def test_set_business_key_ValueError(self, test_data):
+    def test_add_bk_for_table(self, test_data):
         df = test_data.combine("str_normal", "int_normal").get_df()
-        with pytest.raises(ValueError, match="Must provide either bk_name or table_name"):
-            set_business_key(df, "str_normal", "int_normal")
+        table = "correct"
+        df[f"bk_{table}"] = add_bk_for_table(table, df, "str_normal", "int_normal")
 
-    def test_set_business_key_custom_deault(self, test_data):
-            df = test_data.combine("str_normal", "int_normal").get_df()
-            df_result = set_business_key(df, "str_normal", "int_normal", table_name="user")
-            expected = (df["str_normal"].astype(str) + "||" + df["int_normal"].astype(str))
-            expected.name = "bk_user"
-        
-            pd.testing.assert_series_equal(df_result, expected)
+    def test_add_bk_for_table_mixed_col(self, test_data):
+        df = test_data.combine("str_normal", "int_mixed").get_df()
+        table = "correct"
+        df_result = add_bk_for_table(table, df, "str_normal", "int_mixed")
+        expected = (df["str_normal"].astype(str) + "||" + df["int_mixed"].astype(str))
+        expected.name = "bk_correct"
 
-    def test_set_business_key_costum_bk_name(self, test_data):
-            df = test_data.combine("str_normal", "int_normal").get_df()
-            df_result = set_business_key(df, "str_normal", "int_normal", bk_name="bk_cust")
-            expected = (df["str_normal"].astype(str) + "||" + df["int_normal"].astype(str))
-            expected.name = "bk_cust"
-        
-            pd.testing.assert_series_equal(df_result, expected)
-            assert expected.name in df_result.name
-
-    def test_set_business_key_mixed_col(self, test_data):
-            df = test_data.combine("str_normal", "int_mixed").get_df()
-            df_result = set_business_key(df, "str_normal", "int_mixed", table_name="user")
-            expected = (df["str_normal"].astype(str) + "||" + df["int_mixed"].astype(str))
-            expected.name = "bk_user"
-
-            pd.testing.assert_series_equal(df_result, expected)
+        pd.testing.assert_series_equal(df_result, expected)
